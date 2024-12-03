dced6a4c8493ffa5faa374bd245bb47076f05c4a
FattiMei <teomei68@gmail.com>
2024-12-03 15:08:04 +0100

    Add a more complete testing procedure of the algorithm

--- regression.cpp
+++ regression.cpp
@@ -1,46 +1,51 @@
-#include <array>
+#include <vector>
 #include <cstdlib>
 #include <cassert>
 #include <iostream>
 #include "automata.hpp"
 
 
-constexpr int size = 16;
+#define TEST_IMPLEMENTATION(foo) assert(test_implementation(foo))
+
+
+constexpr int size = 100;
 constexpr int rule = 103;
-using Line = std::array<Cell, size>;
+using Line = std::vector<Cell>;
+
+
+bool test_implementation(void (*impl) (const int, const Cell[], Cell[], const int)) {
+	Line start(size);
+	Line reference(size);
+	Line alternative(size);
+
+	for (int n = 3; n < size; ++n) {
+		const int rule = std::rand() % 256;
+
+		for (int i = 0; i < size; ++i) {
+			start[i] = std::rand() % 2;
+		}
+
+		compute_new_line_reference(n, start.data(), reference.data(), rule);
+		impl(n, start.data(), alternative.data(), rule);
+
+		for (int i = 0; i < size; ++i) {
+			if (reference[i] != alternative[i]) {
+				std::cerr << "Found error when size = " << n << std::endl;
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
 
 
 int main() {
-	Line start;
-	Line reference;
-	Line alternative;
-
-	for (int i = 0; i < size; ++i) {
-		start[i] = std::rand() % 2;
-	}
-
-	compute_new_line_reference(size, start.data(), reference.data(), rule);
-
-	compute_new_line_branchless(size, start.data(), alternative.data(), rule);
-	assert(reference == alternative);
-
-	compute_new_line_corner_cases(size, start.data(), alternative.data(), rule);
-	assert(reference == alternative);
-
-	compute_new_line_reuse(size, start.data(), alternative.data(), rule);
-	assert(reference == alternative);
-
-	compute_new_line_full_reuse(size, start.data(), alternative.data(), rule);
-	assert(reference == alternative);
-
-	compute_new_line_packed(size, start.data(), alternative.data(), rule);
-
-	for (auto x : reference) std::cout << (int) x << ' ';
-	std::cout << std::endl;
-	for (auto x : alternative) std::cout << (int) x << ' ';
-	std::cout << std::endl;
-
-	// assert(reference == alternative);
+	TEST_IMPLEMENTATION(compute_new_line_reference);
+	TEST_IMPLEMENTATION(compute_new_line_branchless);
+	TEST_IMPLEMENTATION(compute_new_line_corner_cases);
+	TEST_IMPLEMENTATION(compute_new_line_reuse);
+	TEST_IMPLEMENTATION(compute_new_line_full_reuse);
 
 	return 0;
 }
