b91a861a7d22e76a68ebc301cf2a4df4b254d486
FattiMeiPi <teomei68@gmail.com>
2024-12-04 22:40:42 +0100

    hotfix: remove magic numbers in SIMD implementation

--- automata.cpp
+++ automata.cpp
@@ -178,10 +178,10 @@ void compute_new_line_simd(const int n, const Cell above[], Cell below[], const 
 		vector.copy_from(above + i, std::experimental::vector_aligned);
 		right = above[i+SIZEOF];
 
-		lshift = simd_t([vector](int i) {return vector[(i+1) % 16];});
+		lshift = simd_t([vector](int i) {return vector[(i+1) % SIZEOF];});
 		lshift[0] = left;
 
-		rshift = simd_t([vector](int i) {return vector[(i-1) % 16];});
+		rshift = simd_t([vector](int i) {return vector[(i-1) % SIZEOF];});
 		rshift[SIZEOF-1] = right;
 
 		acc = 4*lshift + 2*vector + rshift;
